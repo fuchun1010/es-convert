efbacea81b3036a2ef33911f4bb601d32f507076
FuChun <fuchun198435@163.com>
2017-06-03 09:18:15 +0800

    [fuchun] #Tech refactor agg generator

--- common/criteria-to-es.js
+++ common/criteria-to-es.js
@@ -1,47 +1,49 @@
+const toAgg = (bindingItems, query) => {
+  let currentCategoryField
+  bindingItems.forEach(item => {
+    if(item.bindingType === 'Category') {
+        let fieldName = global.fields.get(item.fieldId)
+        currentCategoryField = fieldName
+        if(!query.aggs)
+          query.aggs = {}
+        query.aggs = {
+          [fieldName]: {
+            terms:{
+              field: fieldName,
+              size: 5000
+            }
+          }
+        }
+      }
+      else {
+        const {bindingFunction: {functionName, parameters}} = item
+        let currentAgg = query.aggs[currentCategoryField]
+        //这个地方要考虑一下多个参数的情况
+        let aggField = parameters && parameters[0] && global.fields.get(parameters[0].fieldId)
+        if(!currentAgg.aggs) {
+          currentAgg.aggs = {}
+        }
+        currentAgg.aggs[aggField] = {
+          [functionName]: {
+              field: aggField
+          }
+        }
+      }
+  })
+  return {size:0, ...query}
+}
+
+
+
 const transform = (analysisRequest) => {
+
   if(!analysisRequest)
     return {}
   
   const {dataSetId, bindingItems, filter, selectors} = analysisRequest
 
-  const toAgg = (bindingItems, query) => {
-    let currentCategoryField
-    bindingItems.forEach(item => {
-      if(item.bindingType === 'Category') {
-          let fieldName = global.fields.get(item.fieldId)
-          currentCategoryField = fieldName
-          if(!query.aggs)
-            query.aggs = {}
-          query.aggs = {
-            [fieldName]: {
-              terms:{
-                field: fieldName,
-                size: 5000
-              }
-            }
-          }
-        }
-        else {
-          const {bindingFunction: {functionName, parameters}} = item
-          debugger
-          let currentAgg = query.aggs[currentCategoryField]
-          //这个地方要考虑一下多个参数的情况
-          let aggField = parameters && parameters[0] && global.fields.get(parameters[0].fieldId)
-          if(!currentAgg.aggs) {
-            currentAgg.aggs = {}
-          }
-          currentAgg.aggs[aggField] = {
-            [functionName]: {
-                field: aggField
-            }
-          }
-        }
-    })
-    return query
-  }
-
-  const result = toAgg(bindingItems, {})
-  return result
+  const aggs = toAgg(bindingItems, {})
+  return aggs
 
 }
 
